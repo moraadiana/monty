410a097630c4f8ab6faa9912ab7cf6b03dbd0d16
moraadiana13 <moraadiana13@gmail.com>
2023-09-20 23:10:07 -0700

    Stacks, Queues - LIFO, FIFO

---
+++ stack_func1.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+
+/**
+ * add_to_stack - Add node to stack.
+ * @new_node: Points to the new node.
+ * @ln: is an nterger representing the line number of opcode.
+ */
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
+}
+
+
+/**
+ * print_stack - Add a node to the stack.
+ * @stack: is a pointer to a pointer that points to top node of the stack.
+ * @line_number: is the line number of opcode.
+ */
+void print_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void) line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+
+/**
+ * pop_top - use to adds node to stack.
+ * @stack: is a pointer to a pointer that points top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void pop_top(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_number);
+
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
+}
+
+/**
+ * print_top - used to prints the top node of the stack.
+ * @stack: is a pointer to a pointer that points top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void print_top(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		more_err(6, line_number);
+	printf("%d\n", (*stack)->n);
+}
